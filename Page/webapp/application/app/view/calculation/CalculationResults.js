865578f5756e3389698a434085803c05ce6ffc21
kornicameister <kornicameister@gmail.com>
2014-02-08 18:18:14 +0100

    Added some constant fields with tooltip

--- Page/webapp/application/app/view/calculation/CalculationResults.js
+++ Page/webapp/application/app/view/calculation/CalculationResults.js
@@ -5,16 +5,121 @@ Ext.define('CE.view.calculation.CalculationResults', {
         'CE.constants.RenderFunctions'
     ],
     defaults: {
-        labelWidth: 100
+        xtype   : 'container',
+        layout  : 'hbox',
+        defaults: {
+            labelWidth: 100,
+            value     : 0
+        }
     },
     items   : [
         {
-            xtype     : 'displayfield',
-            itemId    : 'carnotEfficiency',
-            fieldLabel: 'Efficiency',
-            renderer  : function (value) {
-                return CE.constants.RenderFunctions.renderAsPercent(value);
-            }
+            items: [
+                {
+                    xtype     : 'displayfield',
+                    itemId    : 'carnotEfficiency',
+                    fieldLabel: 'Efficiency',
+                    renderer  : function (value) {
+                        return CE.constants.RenderFunctions.renderAsPercent(value);
+                    },
+                    flex      : 5
+                },
+                {
+                    xtype  : 'button',
+                    text   : '&#9830;',
+                    flex   : 1,
+                    handler: function () {
+                        Ext.Msg.show({
+                            title: 'Info',
+                            msg  : 'Sprawność silnika Carnot zależy jedynie od temperatur'
+                        });
+                    }
+                }
+            ]
+        },
+        {
+            items: [
+                {
+                    xtype     : 'displayfield',
+                    fieldLabel: '&#916;T',
+                    value     : 0,
+                    flex      : 5
+                },
+                {
+                    xtype  : 'button',
+                    text   : '&#9830;',
+                    flex   : 1,
+                    handler: function () {
+                        Ext.Msg.show({
+                            title: 'Info',
+                            msg  : '&#916;T w cyklu jest stała i wynosi 0'
+                        });
+                    }
+                }
+            ]
+        },
+        {
+            items: [
+                {
+                    xtype     : 'displayfield',
+                    fieldLabel: '&#916;H',
+                    value     : 0,
+                    flex      : 5
+                },
+                {
+                    xtype  : 'button',
+                    text   : '&#9830;',
+                    flex   : 1,
+                    handler: function () {
+                        Ext.Msg.show({
+                            title: 'Info',
+                            msg  : '&#916;H w cyklu jest stała i wynosi 0'
+                        });
+                    }
+                }
+            ]
+        },
+        {
+            items: [
+                {
+                    xtype     : 'displayfield',
+                    fieldLabel: '&#916;U',
+                    value     : 0,
+                    flex      : 5
+                },
+                {
+                    xtype  : 'button',
+                    text   : '&#9830;',
+                    flex   : 1,
+                    handler: function () {
+                        Ext.Msg.show({
+                            title: 'Info',
+                            msg  : '&#916;U w cyklu jest stała i wynosi 0'
+                        });
+                    }
+                }
+            ]
+        },
+        {
+            items: [
+                {
+                    xtype     : 'displayfield',
+                    fieldLabel: '&#916;S',
+                    value     : 0,
+                    flex      : 5
+                },
+                {
+                    xtype  : 'button',
+                    text   : '&#9830;',
+                    flex   : 1,
+                    handler: function () {
+                        Ext.Msg.show({
+                            title: 'Info',
+                            msg  : '&#916;S w cyklu jest stała i wynosi 0'
+                        });
+                    }
+                }
+            ]
         }
     ]
 });
